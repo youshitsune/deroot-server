56ade250af8406d4fca1b1d1652cfb4c73515662
youshitsune <youshitsune@dmz.rs>
2023-07-29 07:12:24 +0200

    remove rsa encryption

--- main.py
+++ main.py
@@ -1,23 +1,12 @@
 import threading
 import socket
 import os 
-import rsa
 import pathlib
 
 HEADER = 70000
 IP = "0.0.0.0"
 PORT = 5050
 content_dir = "ctx/"
-
-if os.path.exists("private_key.pem") and os.path.exists("public_key.pem"):
-    pub_key = rsa.PublicKey.load_pkcs1(open("public_key.pem", "rb").read())
-    priv_key = rsa.PrivateKey.load_pkcs1(open("private_key.pem", "rb").read())
-else:
-    pub_key, priv_key = rsa.newkeys(2048)
-    with open("public_key.pem", "wb") as f:
-        f.write(pub_key.save_pkcs1("PEM"))
-    with open("private_key.pem", "wb") as f:
-        f.write(priv_key.save_pkcs1("PEM"))
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((IP, PORT))
@@ -35,23 +24,18 @@ def generate_index():
 
 
 def handle_req(client):
-    client.send(str(len(pub_key.save_pkcs1("PEM"))).encode() + b' ' * (HEADER - len(str(len(pub_key.save_pkcs1("PEM"))).encode())))
-    client.send(pub_key.save_pkcs1("PEM"))
-    size = client.recv(HEADER).decode()
-    if size:
-        pub_con = rsa.PublicKey.load_pkcs1(client.recv(int(size)))
     while True:
-        size = rsa.decrypt(client.recv(HEADER), priv_key).decode()
+        size = client.recv(HEADER).decode()
         if size:
-            req = rsa.decrypt(client.recv(int(size)), priv_key).decode()
+            req = client.recv(int(size)).decode()
             if os.path.exists(content_dir + req):
                 with open(content_dir + req, "rb") as f:
                     ctx = f.read()
-                client.send(rsa.encrypt(str(len(ctx)).encode() + b' ' * (HEADER - len(str(len(ctx)).encode())), pub_con))
-                client.send(rsa.encrypt(ctx), pub_con)
+                client.send(str(len(ctx)).encode() + b' ' * (HEADER - len(str(len(ctx)).encode())))
+                client.send(ctx)
             else:
-                client.send(rsa.encrypt(str(len(b"404")).encode() + b' ' * (HEADER - len(str(len(b"404")).encode())), pub_con))
-                client.send(rsa.encrypt(b"404"), pub_con)
+                client.send(str(len(b"404")).encode() + b' ' * (HEADER - len(str(len(b"404")).encode())))
+                client.send(b"404")
 
 def run():
     generate_index()
